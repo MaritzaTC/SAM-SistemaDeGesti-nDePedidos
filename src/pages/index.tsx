38e870cc504cba55ec67d41b4be1937810961eca
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-07-13 21:11:29 -0500

    feat: update Home component to include Hero, FeaturedProducts, and Features sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
+import Footer from "@/components/client/Footer";
+import FeaturedProducts from "@/components/client/FeaturedProducts/intex";
+import Features from "@/components/client/Features";
+import Hero from "@/components/client/Hero";
 
+//import DashboardE from "@/components/admin/DashboardE";
 
 const Home = () => (
-  <main>Bienvenido a  SAM 👗</main>
+  <main >
+
+    {/* Admin Section */}
+    {/* <DashboardE></DashboardE> */}
+    
+    {/* Uncomment the following lines to include client components */}
+    
+    {/* Client Section */}
+    <Hero></Hero>
+    <FeaturedProducts></FeaturedProducts>
+    <Features></Features>
+    <Footer></Footer>
+  </main>
 );
+
 Home.noAuth = true;
-Home.noLayout = true;
+Home.noLayout = false;
+
 export default Home;
