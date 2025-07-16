272a7aa1da6fb0170e0402ef2b01008dd31d97cf
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-07-16 14:06:04 -0500

    refactor: enhance _app component to support custom layouts and improve type definitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
 
-type CustomAppProps = AppProps & {
-  Component: AppProps['Component'] & {
-    noAuth?: boolean;
-    noLayout?: boolean;
-  };
+// Tipo para permitir `getLayout`
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+// Nuevo _app
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: CustomAppProps) {
-  const isPublic = Component.noAuth ?? false;
-  const useLayout = !Component.noLayout;
+}: AppPropsWithLayout) {
+  // Usa el layout definido por la página o aplica el layout por defecto
+  const getLayout =
+    Component.getLayout ??
+    ((page) => <Layout requireAuth={!(Component as any).noAuth}>{page}</Layout>);
 
-  const content = useLayout ? (
-    <Layout requireAuth={!isPublic}>
-      <Component {...pageProps} />
-    </Layout>
-  ) : (
-    <Component {...pageProps} />
-  );
-
-  return isPublic ? content : (
+  return (
     <SessionProvider session={session}>
-      {content}
+      {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
   );
 }
